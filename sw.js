6bdf7b9f28d18e95ae93daf657ff703e8ed70a99
1101cody <cody.douma@gmail.com>
2026-06-25 18:52:17 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'ireland-golf-v1';
+const ASSETS = ['./ireland_golf_2026.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Network first for Supabase API calls
+  if(e.request.url.includes('supabase.co')) return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
